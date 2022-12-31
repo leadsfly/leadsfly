d3dd12fc5eb1570c9e4b0c8126aa3f2438460f1d
Rambo <rambo1234@example.com>
2022-12-31 08:32:08 +0530

    done hero session final7

--- scenes/MySkills.tsx
+++ scenes/MySkills.tsx
@@ -19,7 +19,7 @@ function MySkills() {
           viewport={{ once: true, amount: 0.5 }}
           transition={{ duration: 0.5 }}
           variants={{
-            hidden: { opacity: 0, x: -50 },
+            hidden: { opacity: 0, x: 0 },
             visible: { opacity: 1, x: 0 },
           }}
         >
